24f0501ed3426cf52e315dbdf46a057afc32f580
Alex Kallam <aakallam@gmail.com>
2014-11-16 19:00:23 -0500

    Added LogLikelihood calculation

--- Implementation/Helper.cpp
+++ Implementation/Helper.cpp
@@ -13,3 +13,35 @@ vector<int>* randomLociCalc(vector<string>* sequences,int k) {
 
 }
 
+double calculateLogLikelyhood(int numberOfSequences,Probability prob,Profile prof, char* motif) {
+    double ll = 0;
+    int i, prof_val, len;
+    double prob_val;
+    len = strlen(motif);
+    for (i = 0; i < len; i++) {
+        if (motif[i] == '*') {
+            continue;
+        }
+        else if (motif[i] == 'a') {
+            prof_val = prof.getCount('a', i);
+            prob_val = prob.getProbability('a');
+        }
+        else if (motif[i] == 't') {
+            prof_val = prof.getCount('t', i);
+            prob_val = prob.getProbability('t');
+        }
+        else if (motif[i] == 'g') {
+            prof_val = prof.getCount('g', i);
+            prob_val = prob.getProbability('g');
+        }
+        else if (motif[i] == 'c') {
+            prof_val = prof.getCount('c', i);
+            prob_val = prob.getProbability('c');
+        }
+        else {
+            printf("ERROR in calculating loglikelihood\n");
+        }
+        ll += ((prof_val / numberOfSequences) / prob_val);
+    }
+    return ll;
+}
