fa944ef8f6cda31a0b48f319044c4172178b34d8
David Sussman <davsuss@vt.edu>
2014-12-12 15:08:03 -0500

    Added Concurrent Queue

---
+++ Implementation/Queue.h
@@ -0,0 +1,38 @@
+
+#include <thread>
+#include <mutex>
+#include <queue>
+
+#ifndef QUEUEH
+#define QUEUEH
+
+
+using namespace std;
+template <class T>
+class ConcurrentQueue {
+
+std::mutex _queue_mutex;
+std::queue<T> _queue;
+
+ConcurrentQueue() {
+
+_queue_mutex = new std::mutex();
+_queue = new std::queue<T>();
+}
+
+void addItem(T t){
+
+std::lock_guard<std::mutex> lock(_queue_mutex);
+_queue.push_front(t);
+
+}
+
+T removeItem() {
+std::lock_guard<std::mutex> lock(_queue_mutex);
+return _queue.pop_back();
+}
+
+};
+
+
+#endif
